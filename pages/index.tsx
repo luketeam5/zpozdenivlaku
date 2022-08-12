ce4ac7fd6071cecefcceb1e1106864d7b83ba537
Peperman66 <peperman66@seznam.cz>
2022-08-12 22:12:37 +0200

    add SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,16 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
+import useSWR from 'swr'
 import { DelayTable } from '../components/DelayTable'
 import { DelayInfo } from '../types/DelayInfo'
 
 const Home: NextPage = () => {
-  const [delayData, setDelayData] = useState<DelayInfo | null>(null)
-  async function refreshData(): Promise<NodeJS.Timeout> {
-    const res = await fetch('/api/getDelays')
-    const data = await res.json()
-    setDelayData(data)
-    return setTimeout(refreshData, 60*11*1000)
-  }
-  if (delayData == null) {
-    refreshData()
-  }
+  const {data} = useSWR('/api/getDelays', (...args) => fetch(...args).then(res => res.json() as Promise<DelayInfo>), {refreshInterval: 10*60*1000})
   return (
     <div className='flex flex-col items-center'>
       <h1 className='text-2xl font-bold'>Průměrné zpoždění vlaků železničních dopravců v ČR</h1>
-      {"Naposledy aktualizováno: " + (delayData?.timeFetched ? /*new Date(delayData.timeFetched)*/ delayData.timeFetched : "nikdy")}
-      {delayData && <DelayTable companyInfos={delayData?.companies}/>}
+      {"Naposledy aktualizováno: " + (data?.timeFetched ? /*new Date(delayData.timeFetched)*/ data.timeFetched : "nikdy")}
+      {data && <DelayTable companyInfos={data?.companies}/>}
     </div>
   )
 }
