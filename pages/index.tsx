9dc927f967cbbac15211db3d6e7feea5c8bae68a
Peperman66 <peperman66@seznam.cz>
2022-08-15 01:35:08 +0200

    add loading text even when revalidating

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ import { DelayTable } from '../components/DelayTable'
 import { DelayInfo } from '../types/DelayInfo'
 
 const Home: NextPage = () => {
-  const {data} = useSWR('/api/getDelays', (...args) => fetch(...args).then(res => res.json() as Promise<DelayInfo>), {refreshInterval: 10*60*1000})
+  const {data, isValidating} = useSWR('/api/getDelays', (...args) => fetch(...args).then(res => res.json() as Promise<DelayInfo>), {refreshInterval: 10*60*1000})
   return (
     <div className='flex flex-col items-center min-w-min'>
       <h1 className='text-center text-2xl font-bold'>Průměrné zpoždění vlaků železničních dopravců v ČR</h1>
       <span>{"Naposledy aktualizováno: " + (data?.timeFetched ? /*new Date(delayData.timeFetched)*/ data.timeFetched : "nikdy")}</span>
-      {!data && (<span>Načítání...</span>)}
+      {!data || isValidating && (<span>Načítání...</span>)}
       {data && <DelayTable companyInfos={data?.companies}/>}
     </div>
   )
